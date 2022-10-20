a5a56f56b8de820eb9589cc0642a2a71bae79197
Abysinia <abysiniapro@gmail.com>
2022-10-20 13:33:52 -0400

    remover files

---
+++ Python/hello.py
@@ -0,0 +1,24 @@
+print("hello")
+month = ['one', 'two', 'three', 'four']
+str = "Hello there"
+
+print(month[-2])
+print(month[len(month)-3])
+print(month[2])
+print(month[1:3])
+print(month[:2])
+print(month[2:])
+print('two' in month)
+print('Hello' in str)
+print('Hello' not in str)
+print(len(month))
+print(len(str))
+print(f"{98} Battery street")
+print(f"{98} Battery street, {'San Francisco'}")
+a = "Python is cool"
+print(a[4])
+print(a[0:6])
+print(a[:6])
+print(a[7:])
+print(a[7:-5])
+print(a[-2])
